460a8d9c428635ef15803aa4c9a2e8c10c30774d
ohjacksn <jacksonmo@me.com>
2020-05-12 11:56:58 -0400

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    @time = Time.new
+    erb :index
+  end
+
+end
